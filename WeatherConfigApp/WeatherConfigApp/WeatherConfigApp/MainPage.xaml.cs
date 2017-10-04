99687b1ae49939b395a00272f7ef42f689b49a59
MohamadSoufan <ms584@evansville.edu>
2017-10-04 17:35:11 -0500

    init commit after first build

---
+++ WeatherConfigApp/WeatherConfigApp/WeatherConfigApp/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace WeatherConfigApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private async void BluetoothButton_Clicked(object sender, EventArgs e)
+        {
+            var boo = await DisplayAlert("haha", "Bluetooth it is", "Great!", "shit");
+
+            if (boo)
+                EntryField.Text = "Changed";
+            else
+                EntryField.Text = "Not Changed";
+
+        }
+        
+    }
+}
