8ad2c80b090f1a2015ed272902ac3f44a7e7ee76
MohamadSoufan <ms584@evansville.edu>
2017-11-28 16:06:24 -0600

    Added advertise handler

--- WeatherConfigApp/WeatherConfigApp/WeatherConfigApp/Pages/ConnectDevice.xaml.cs
+++ WeatherConfigApp/WeatherConfigApp/WeatherConfigApp/Pages/ConnectDevice.xaml.cs
@@ -15,6 +15,7 @@ namespace WeatherConfigApp.Pages
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ConnectDevice : ContentPage
     {
+        public string UuidString { get; set; }
         public IBluetoothLE BluetoothLe { get; set; }
         public IAdapter Adapter { get; set; }
         public IDevice BtDevice { get; set; }
@@ -27,11 +28,27 @@ namespace WeatherConfigApp.Pages
             Adapter = CrossBluetoothLE.Current.Adapter;
             DeviceList = new ObservableCollection<IDevice>();
             Adapter.DeviceDiscovered += Adapter_DeviceDiscovered;
+  //          Adapter.DeviceAdvertised += Adapter_DeviceAdvertised;
             DevicesListView.ItemsSource = DeviceList;
             DevicesListView.ItemTapped += DevicesListView_ItemTapped;
 
            
         }
+
+        private async void Adapter_DeviceAdvertised(object sender, Plugin.BLE.Abstractions.EventArgs.DeviceEventArgs e)
+        {
+            try
+            {
+                await DisplayAlert("Alert",
+                    "id= " + e.Device.Id+ " ARtype: " +
+                    e.Device.AdvertisementRecords[0].Type, "ok");
+            }
+            catch (Exception exe)
+            {
+                await DisplayAlert("Exception",exe.Message,"cancel");
+            }
+        }
+
         public ConnectDevice(WeatherStation weatherstation):this()
         {
             FatherStation = weatherstation;
@@ -39,29 +56,32 @@ namespace WeatherConfigApp.Pages
 
         private async void DevicesListView_ItemTapped(object sender, ItemTappedEventArgs e)
         {
-            
-            BtDevice = e.Item as IDevice;
-            
+
+            BtDevice = DevicesListView.SelectedItem as IDevice;
+           
             try
             {
                 if(Adapter.IsScanning)
                     await Adapter.StopScanningForDevicesAsync();
                 
-                await Adapter.ConnectToDeviceAsync(BtDevice);
+           //     await Adapter.ConnectToDeviceAsync(BtDevice);
+
+                await Navigation.PushAsync(new NetworkInfoPage(BluetoothLe,Adapter,BtDevice));
 
             }
             catch (DeviceConnectionException exception)
             {
                 await DisplayAlert("Exception", exception.Message, "Exit");
-                return;
             }
            
-            FatherStation.ConnectionStatus = "Connected";
+           // FatherStation.ConnectionStatus = "Connected";
         }
 
         private void Adapter_DeviceDiscovered(object sender, Plugin.BLE.Abstractions.EventArgs.DeviceEventArgs e)
         {
-            DeviceList.Add(e.Device);   
+            UuidString = e.Device.Id.ToString();
+            DeviceList.Add(e.Device);
+            
         }
 
         private async void ScanBtn_Clicked(object sender, EventArgs e)
--- WeatherConfigApp/WeatherConfigApp/WeatherConfigApp/Pages/NetworkInfoPage.xaml.cs
+++ WeatherConfigApp/WeatherConfigApp/WeatherConfigApp/Pages/NetworkInfoPage.xaml.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Plugin.BLE.Abstractions;
+using Plugin.BLE.Abstractions.Contracts;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,9 +13,59 @@ namespace WeatherConfigApp.Pages
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class NetworkInfoPage : ContentPage
 	{
-		public NetworkInfoPage ()
+	    public static string NetworkSsid;
+	    public static string NetworkPwd;
+	    private IBluetoothLE BluetoothLe { get;}
+        private IAdapter Adapter { get; }
+	    private IDevice BtDevice { get; }
+	    private IService Service { get; set; }
+
+	    public NetworkInfoPage ()
 		{
 			InitializeComponent ();
+		    NetworkPwdEntry.Text = "";
+		    NetworkSsidEntry.Text = "";
 		}
-	}
+
+	    public NetworkInfoPage(IBluetoothLE bluetoothLe, IAdapter adapter, IDevice device):this()
+	    {
+	        BluetoothLe = bluetoothLe;
+	        Adapter = adapter;
+	        BtDevice = device;
+	    }
+        private async void SendBtn_Clicked(object sender, EventArgs e)
+        {
+            NetworkSsid = NetworkSsidEntry.Text;
+            NetworkPwd = NetworkPwdEntry.Text;
+
+            try
+            {
+                var services = await BtDevice.GetServicesAsync();
+            }
+            catch (Exception exception)
+            {
+                await DisplayAlert("Exception", exception.Message, "Exit");
+            }
+
+
+            //await Navigation.PopAsync();
+        }
+
+        private void NetworkSsidEntry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateUi();
+        }
+
+	    private void NetworkPwdEntry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateUi();
+        }
+	    private void UpdateUi()
+	    {
+	        if (NetworkPwdEntry.Text.Length > 0 && NetworkPwdEntry.Text.Length > 7)
+	            SendBtn.IsEnabled = true;
+	        else
+	            SendBtn.IsEnabled = false;
+	    }
+    }
 }
